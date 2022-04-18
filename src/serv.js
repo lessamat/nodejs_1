316171ea66f1654b5e6137eb5f156c365e648699
lessamat <lessal180@gmail.com>
2022-04-18 17:39:42 -0300

    adicionando post

--- src/serv.js
+++ src/serv.js
@@ -8,6 +8,19 @@ const port = 4001
 server.use(json())
 server.use(cors())
 
+server.get('/', (req, res) => {
+    res.send("Suquinho xuáááá huuuum\n")
+})
+
+server.post('/', (req, res) => {
+
+    server.post('/', (req, res) => {
+        const request = req.body
+        res.send(request.usuario)
+        res.status(201).send();
+    })
+})
+
 server.listen(port, (req, res) => {
     console.log(`server running on port ${port}`)
 })
